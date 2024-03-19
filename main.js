e6cf416c169a727ccd8e92d97f60c1960c5dd2f7
David Wilkinson <dbw200001@utdallas.edu>
2024-03-19 15:30:08 -0500

    Set up EJS

--- main.js
+++ main.js
@@ -2,11 +2,46 @@ const bodyParser = require('body-parser');
 const express = require("express");
 
 const schemas = require("./schemas");
+const httpStatus = require("./http_status");
+// console.debug(httpStatus);
 
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/users", (req, res) => {
+    res.render("allUsersList.ejs");
+})
+
+app.get("/user", (req, res) => {
+    res.render("user.ejs");
+})
+
+app.get("/profile", (req, res) => {
+    res.render("profile.ejs");
+})
+
+app.get("/resumeContact", (req, res) => {
+    res.render("resumeContactForm.ejs");
+})
+
+app.get("/submitPreferences", (req, res) => {
+    res.render("submitPreferences.ejs");
+})
+
+app.get("/teams", (req, res) => {
+    res.render("team-list.ejs");
+})
+
+app.get("/invites", (req, res) => {
+    res.render("invite-inbox.ejs");
+})
 
 app.post("/submitPreferences", urlencodedParser, (req, res) => {
     console.log("Submit preferences request:", req.body);
@@ -14,21 +49,21 @@ app.post("/submitPreferences", urlencodedParser, (req, res) => {
 
     const result = schemas.preferences.validate(req.body);
     if (result.error)
-        return res.status(400).send(result.error.details[0].message);
+        return res.status(httpStatus.BAD_REQUEST).send(result.error.details[0].message);
 
     // Ensure no preferences are repeated among external and CS projects
     const prefs = Object.values(result.value);
     prefs.sort();
     for (var i = 1; i < prefs.length; i++) {
         if (prefs[i - 1] === prefs[i])
-            return res.status(400).send("Preferences must be different projects");
+            return res.status(httpStatus.BAD_REQUEST).send("Preferences must be different projects");
     }
 
     // TODO: Update data storage for preferences
 
     // Send the browser to the user's own page to view new preferences
     // TODO: Update this URL when that gets set up
-    res.redirect("/user.html");
+    res.redirect("/user");
 });
 
 app.post("/invites/:teamid/respond", urlencodedParser, (req, res) => {
@@ -37,14 +72,14 @@ app.post("/invites/:teamid/respond", urlencodedParser, (req, res) => {
 
     const result = schemas.inviteResponse.validate(req.body);
     if (result.error)
-        return res.status(400).send(result.error.details[0].message);
+        return res.status(httpStatus.BAD_REQUEST).send(result.error.details[0].message);
 
     // TODO: Ensure user is not on a team if accepting
     // TODO: Apply team change
 
     // Send the user to their new team's page
     // TODO: Update this URL when that gets set up
-    res.redirect("/team.html");
+    res.redirect("/team");
 });
 
 const port = process.env.PORT || 3000;
