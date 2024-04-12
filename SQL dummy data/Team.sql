e45787e9eb7e5959abc4ac118d642feb9bee833a
David Wilkinson <dbw200001@utdallas.edu>
2024-04-11 21:20:56 -0500

    Remove empty teams from dummy data

--- SQL dummy data/Team.sql
+++ SQL dummy data/Team.sql
@@ -7,14 +7,3 @@ values (3, 1);
 insert into Team (teamID, projectID)
 values (4, null);
 insert into Team (teamID, projectID)
-values (5, null);
-insert into Team (teamID, projectID)
-values (6, null);
-insert into Team (teamID, projectID)
-values (7, null);
-insert into Team (teamID, projectID)
-values (8, null);
-insert into Team (teamID, projectID)
-values (9, null);
-insert into Team (teamID, projectID)
-values (10, null);
